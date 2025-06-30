451d75628cd026646e80699430f77cfc991dd2f8
Gaurang_02 <heygaurang.02@gmail.com>
2025-06-30 08:18:31 +0530

    minor changes

--- Backend/app.js
+++ Backend/app.js
@@ -1,34 +1,34 @@
-const dotenv=require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
 const app = express();
-const cors=require('cors');
-const connecToDB=require('./db/db');
-const userRoutes=require('./routes/user.routes')
-const cookieParser = require('cookie-parser');
-const captainRoutes = require('./routes/captain.routes');
-const mapsRoutes = require('./routes/maps.routes');
-const rideRoutes = require('./routes/ride.routes');
-const twilioRoutes = require('./routes/twilio.routes');
+const cors = require("cors");
+const connecToDB = require("./db/db");
+const userRoutes = require("./routes/user.routes");
+const cookieParser = require("cookie-parser");
+const captainRoutes = require("./routes/captain.routes");
+const mapsRoutes = require("./routes/maps.routes");
+const rideRoutes = require("./routes/ride.routes");
+const twilioRoutes = require("./routes/twilio.routes");
 
-app.use(cors());
+app.use({
+  origin: ["https://your-vercel-app.vercel.app"],
+  credentials: true,
+});
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 connecToDB();
-
 
 // app.get("/", (req, res) => {
 //   res.send("hello world");
 // });
 
-
-
-app.use('/users',userRoutes);
-app.use('/captains', captainRoutes);
-app.use('/maps', mapsRoutes);
-app.use('/rides', rideRoutes);
-app.use('/twilio', twilioRoutes);
+app.use("/users", userRoutes);
+app.use("/captains", captainRoutes);
+app.use("/maps", mapsRoutes);
+app.use("/rides", rideRoutes);
+app.use("/twilio", twilioRoutes);
 
 module.exports = app;

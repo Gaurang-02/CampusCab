60ac508592796c454992210561fba17d6f6a76be
Gaurang_02 <heygaurang.02@gmail.com>
2025-06-30 08:21:10 +0530

    minors

--- Backend/socket.js
+++ Backend/socket.js
@@ -1,69 +1,69 @@
-const socketIo = require('socket.io');
-const userModel = require('./models/user.model');
-const captainModel = require('./models/captain.model');
+const socketIo = require("socket.io");
+const userModel = require("./models/user.model");
+const captainModel = require("./models/captain.model");
 
 let io;
 
 function initializeSocket(server) {
-    io = socketIo(server, {
-        cors: {
-            origin: '*',
-            methods: [ 'GET', 'POST' ]
-        }
-    });
+  io = socketIo(server, {
+    cors: {
+      origin: "https://campus-cab.vercel.app/", // Use your actual Vercel domain
+      methods: ["GET", "POST"],
+      credentials: true,
+    },
+  });
 
-    io.on('connection', (socket) => {
+  io.on("connection", (socket) => {
     console.log(`Client connected: ${socket.id}`);
 
     // User joins their room
-    socket.on('join', (data) => {
-        const { userId, userType } = data;
+    socket.on("join", (data) => {
+      const { userId, userType } = data;
 
-        socket.join(userId);
-        console.log(`User ${userType} with ID ${userId} joined room ${userId}`);
+      socket.join(userId);
+      console.log(`User ${userType} with ID ${userId} joined room ${userId}`);
     });
 
     // Example: when the driver accepts a ride
-    socket.on('driver-accept-ride', (rideData) => {
-        const { userId, pickup, destination } = rideData;
+    socket.on("driver-accept-ride", (rideData) => {
+      const { userId, pickup, destination } = rideData;
 
-        io.to(userId).emit('ride-accepted', { pickup, destination });
-        console.log(`Sent ride-accepted to user ${userId}`);
+      io.to(userId).emit("ride-accepted", { pickup, destination });
+      console.log(`Sent ride-accepted to user ${userId}`);
     });
 
     // Example: when the driver rejects the ride
-    socket.on('driver-reject-ride', (rideData) => {
-        const { userId } = rideData;
+    socket.on("driver-reject-ride", (rideData) => {
+      const { userId } = rideData;
 
-        io.to(userId).emit('ride-rejected', { message: 'Ride rejected by driver' });
-        console.log(`Sent ride-rejected to user ${userId}`);
+      io.to(userId).emit("ride-rejected", {
+        message: "Ride rejected by driver",
+      });
+      console.log(`Sent ride-rejected to user ${userId}`);
     });
 
-    socket.on('disconnect', () => {
-        console.log(`Client disconnected: ${socket.id}`);
+    socket.on("disconnect", () => {
+      console.log(`Client disconnected: ${socket.id}`);
     });
-});
-
+  });
 }
 
 // ✅ Add this getter
 function getIO() {
-    if (!io) {
-        throw new Error('Socket.io not initialized!');
-    }
-    return io;
+  if (!io) {
+    throw new Error("Socket.io not initialized!");
+  }
+  return io;
 }
 
 const sendMessageToSocketId = (socketId, messageObject) => {
+  console.log(messageObject);
 
-console.log(messageObject);
+  if (io) {
+    io.to(socketId).emit(messageObject.event, messageObject.data);
+  } else {
+    console.log("Socket.io not initialized.");
+  }
+};
 
-    if (io) {
-        io.to(socketId).emit(messageObject.event, messageObject.data);
-    } else {
-        console.log('Socket.io not initialized.');
-    }
-}
-
-
-module.exports = { initializeSocket, sendMessageToSocketId, getIO  };
+module.exports = { initializeSocket, sendMessageToSocketId, getIO };
